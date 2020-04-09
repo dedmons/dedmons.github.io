7968999153edda9a8858df366edc87292197e0e8
DJ Edmonson <dje_git@fastmail.com>
2020-04-09 16:31:31 -0700

    Add status code

--- analytics.js
+++ analytics.js
@@ -4,8 +4,14 @@ window.addEventListener('load', (event) => {
   	data.userAgent = navigator.userAgent;
   	data.language = navigator.language;
   	data.timezone = Intl.DateTimeFormat().resolvedOptions().timeZone;
-  	data.referrer = document.referrer;
+  	data.referrer = (document.referrer === "") ? "-" : document.referrer;
   	data.location = document.location.pathname;
+
+    if (PAGE_URL === "/404.html") {
+      data.status = 404;
+    }  else {
+      data.status = 200;
+    }
 
   	let timing = window.performance.timing;
   	data.latency = timing.responseEnd - timing.fetchStart;
